8ac0f897c4833f0baf89e4756c107690781ca34d
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-03-21 16:08:08 +0000

    adding image displaying utility

--- segmentation/utils.py
+++ segmentation/utils.py
@@ -1,6 +1,8 @@
 import os
 import glob
 from natsort import natsorted
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def get_images_in_path(folder_path):
@@ -11,3 +13,98 @@ def get_images_in_path(folder_path):
             list.append(file_path)
 
     return natsorted(list)
+
+
+# source and modofied from https://stackoverflow.com/a/67992521
+def img_is_color(img):
+
+    if len(img.shape) == 3:
+        # Check the color channels to see if they're all the same.
+        c1, c2, c3 = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+        if (c1 == c2).all() and (c2 == c3).all():
+            return True
+
+    return False
+
+
+def show_image_list(
+    image_dict: dict = {},
+    list_cmaps=None,
+    grid=False,
+    num_cols=2,
+    figsize=(20, 10),
+    title_fontsize=12,
+):
+    """
+    Shows a grid of images, where each image is a Numpy array. The images can be either
+    RGB or grayscale.
+
+    Parameters:
+    ----------
+    images_dict: dict of image titles and image object
+    list_cmaps: list or None
+        Optional list of cmap values for each image. If None, then cmap will be
+        automatically inferred.
+    grid: boolean
+        If True, show a grid over each image
+    num_cols: int
+        Number of columns to show.
+    figsize: tuple of width, height
+        Value to be passed to pyplot.figure()
+    title_fontsize: int
+        Value to be passed to set_title().
+    """
+
+    list_titles, list_images = list(image_dict.keys()), list(image_dict.values())
+
+    assert isinstance(list_images, list)
+    assert len(list_images) > 0
+    assert isinstance(list_images[0], np.ndarray)
+
+    if list_titles is not None:
+        assert isinstance(list_titles, list)
+        assert len(list_images) == len(list_titles), "%d imgs != %d titles" % (
+            len(list_images),
+            len(list_titles),
+        )
+
+    if list_cmaps is not None:
+        assert isinstance(list_cmaps, list)
+        assert len(list_images) == len(list_cmaps), "%d imgs != %d cmaps" % (
+            len(list_images),
+            len(list_cmaps),
+        )
+
+    num_images = len(list_images)
+    num_cols = min(num_images, num_cols)
+    num_rows = int(num_images / num_cols) + (1 if num_images % num_cols != 0 else 0)
+
+    # Create a grid of subplots.
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+
+    # Create list of axes for easy iteration.
+    if isinstance(axes, np.ndarray):
+        list_axes = list(axes.flat)
+    else:
+        list_axes = [axes]
+
+    for i in range(num_images):
+
+        img = list_images[i]
+        title = list_titles[i] if list_titles is not None else "Image %d" % (i)
+        cmap = (
+            list_cmaps[i]
+            if list_cmaps is not None
+            else (None if img_is_color(img) else "gray")
+        )
+
+        list_axes[i].imshow(img, cmap=cmap)
+        list_axes[i].set_title(title, fontsize=title_fontsize)
+        list_axes[i].grid(grid)
+        list_axes[i].axis("off")
+
+    for i in range(num_images, len(list_axes)):
+        list_axes[i].set_visible(False)
+
+    fig.tight_layout()
+    _ = plt.show()
