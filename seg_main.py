9782b4a55978ea32b23562d6a1544b999684ee96
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-03-25 06:30:19 +0000

    imporving the logging to inform what each run configurations are - processing methods are wrapped toallow auto collection and appending of filter configs

--- seg_main.py
+++ seg_main.py
@@ -16,13 +16,40 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 path = "/home/kyro/Code/SegmentationsFeaturingAndTracking/data/ball_frames"
 
 
+# def store_image_data(image: ImageSegmentation, time: datetime):
+#     """method to store in a text file the image data for processing"""
+#     check_path = os.path.exists(f"process_data/{time}/data.txt")
+#     if not check_path:
+#         with open(f"process_data/{time}/data.txt", "w") as f:
+#             f.write(f"Kblur: {image.data['Kblur']}")
+#             f.write(f"blockSize: {image.data['blockSize']}")
+#             f.write(f"C: {image.data['C']}")
+#             f.write(f"KDil: {image.data['KDil']}")
+#             f.write(f"iDil: {image.data['iDil']}")
+#             f.write(f"KClos: {image.data['KClos']}")
+#             f.write(f"iClos: {image.data['iClos']}")
+#             f.write(f"KOpen: {image.data['KOpen']}")
+#             f.write(f"iOpen: {image.data['iOpen']}")
+#             f.write("\n\n")
+
+
+def store_image_data(image: ImageSegmentation, time: datetime):
+    """method to store in a text file the image data for processing"""
+    check_path = os.path.exists(f"process_data/{time}/data.txt")
+    if not check_path:
+        with open(f"process_data/{time}/data.txt", "w") as f:
+            for entry in image.processing_data:
+                f.write(f"{entry}\n")
+
+
 def main():
     images = get_images_in_path(path)
     time = datetime.now().isoformat("_", timespec="seconds")
     os.mkdir(f"process_data/{time}")
+
     for i, image in tqdm(enumerate(images), desc="Processing..:"):
         image = ImageSegmentation(image)
-
+        image.preprocessing2(image)
         images = {
             "original": image.image,
             "intensity": image.intensity,
@@ -32,8 +59,8 @@ def main():
             "opening": image.opening,
             "floodfill_closing": image.fill,
             "floodfill_opening": image.fill2,
-            "dialte_closing": image.dialte_closing,
-            "dialte_opening": image.dialte_opening,
+            # "dialte_closing": image.dialte_closing,
+            # "dialte_opening": image.dialte_opening,
             "circular_fill": image.fill3,
         }
 
@@ -41,8 +68,10 @@ def main():
         show_image_list(
             image_dict=images,
             figsize=(10, 10),
-            save_path=f"process_data/{time}/img_{i}kv_3_kc_5_its_10",
+            save_path=f"process_data/{time}/img_{i}",
         )
+
+    store_image_data(image, time)
 
 
 if __name__ == "__main__":
--- segmentation/image_segmentation.py
+++ segmentation/image_segmentation.py
@@ -6,93 +6,190 @@ from segmentation.utils import fillhole, fill, fillCirc
 class ImageSegmentation:
     def __init__(self, image_path: str):
         self.image = cv2.imread(image_path)
-        self.preprocessing()
 
-    def preprocessing(self):
-        """grayscaling and smoothing of the images"""
-        # extracting the intensity value of the image
-        self.grayscale = cv2.cvtColor(self.image.copy(), cv2.COLOR_BGRA2GRAY)
-        self.hsv = cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2HSV)
-        (self.hue, self.saturation, self.intensity) = cv2.split(self.hsv)
-        # -- analysis opportunity - intensity vs grayscale --
+        self.processing_data = []
+        # self.data = [
+        #     "Kblur": (3, 3),
+        #     "blockSize": 15,
+        #     "C": 3,
+        #     "KDil": (5, 5),
+        #     "iDil": 7,
+        #     "KClos": (5, 5),
+        #     "iClos": 10,
+        #     "KOpen": (5, 5),
+        #     "iOpen": 7,
+        # ]
 
-        # applying gaussian blurring
-        Kv = 5
-        blur = cv2.GaussianBlur(self.intensity, (Kv, Kv), cv2.BORDER_DEFAULT)
-        # blur = cv2.GaussianBlur(self.grayscale, (Kv, Kv), cv2.BORDER_DEFAULT)
+        # self.preprocessing()
 
-        # applying an adaptive thresholding function
-        # -- analysis opportunity - impact of different thresholding --
-        # self.adaptive_mean_threshold = cv2.adaptiveThreshold(
-        #     src=blur,
-        #     maxValue=255,
-        #     adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
-        #     thresholdType=cv2.THRESH_BINARY,
-        #     blockSize=11,
-        #     C=2,
-        # )
+    def blur(self, image, ksize=(3, 3), iterations=1):
+        """apply gaussian blur to the image"""
+        self.processing_data.append(
+            [f"operation,gblur,kernel:{ksize},iterations:{iterations}"]
+        )
+
+        self.blur = image.copy()
+
+        for _ in range(iterations):
+            self.blur = cv2.GaussianBlur(self.blur, ksize, cv2.BORDER_DEFAULT)
+        return self.blur
+
+    def adaptive_threshold(self, image, blockSize=15, C=3):
+        """apply adaptive threshold to the image"""
+        self.processing_data.append(
+            [f"operation:adaptive_threshold,blockSize:{blockSize},C:{C}"]
+        )
+        image = image.copy()
+
         self.adaptive_gaussian_threshold = cv2.adaptiveThreshold(
-            src=blur,
+            src=image,
             maxValue=255,
             adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
             thresholdType=cv2.THRESH_BINARY,
-            blockSize=11,
-            C=5,
+            blockSize=blockSize,
+            C=C,
         )
+        return self.adaptive_gaussian_threshold
 
-        Kc = 5
+    def dilate(self, image, kernel=(5, 5), iterations=7):
+        """apply dilation to the image"""
+        self.processing_data.append(
+            [f"operation:dilate,kernel:{kernel},iterations:{iterations}"]
+        )
+        image = image.copy()
+
         self.dilation = cv2.dilate(
-            self.adaptive_gaussian_threshold.copy(), (Kc, Kc), iterations=7
+            src=image,
+            kernel=kernel,
+            iterations=iterations,
         )
+        return self.dilation
 
-        # noise reduction in a binary image https://www.researchgate.net/publication/4376361_Noise_removal_and_enhancement_of_binary_images_using_morphological_operations
+    def closing(self, image, kernel=(5, 5), iterations=10):
+        """apply closing to the image"""
+        self.processing_data.append(
+            [f"operation:close,kernel:{kernel},iterations:{iterations}"]
+        )
+        image = image.copy()
 
         self.closing = cv2.morphologyEx(
-            self.adaptive_gaussian_threshold.copy(),
-            cv2.MORPH_CLOSE,
-            (Kc, Kc),
-            iterations=10,
+            src=image,
+            op=cv2.MORPH_CLOSE,
+            kernel=kernel,
+            iterations=iterations,
         )
+        return self.closing
+
+    def opening(self, image, kernel=(5, 5), iterations=7):
+        """apply opening to the image"""
+        self.processing_data.append(
+            [f"operation:open,kernel:{kernel},iterations:{iterations}"]
+        )
+        image = image.copy()
 
         self.opening = cv2.morphologyEx(
-            self.adaptive_gaussian_threshold.copy(),
-            cv2.MORPH_OPEN,
-            (Kc, Kc),
+            src=image,
+            op=cv2.MORPH_OPEN,
+            kernel=kernel,
+            iterations=iterations,
+        )
+        return self.opening
+
+    # # @staticmethod
+    # def preprocessing(self):
+    #     """grayscaling and smoothing of the images"""
+    #     # extracting the intensity value of the image
+    #     self.grayscale = cv2.cvtColor(self.image.copy(), cv2.COLOR_BGRA2GRAY)
+    #     self.hsv = cv2.cvtColor(self.image.copy(), cv2.COLOR_BGR2HSV)
+    #     (self.hue, self.saturation, self.intensity) = cv2.split(self.hsv)
+    #     # -- analysis opportunity - intensity vs grayscale --
+    #
+    #     # applying gaussian blurring
+    #     blur = cv2.GaussianBlur(self.intensity, self.data["Kblur"], cv2.BORDER_DEFAULT)
+    #     for i in range(3):
+    #         blur = cv2.GaussianBlur(blur, self.data["Kblur"], cv2.BORDER_DEFAULT)
+    #
+    #     self.adaptive_gaussian_threshold = cv2.adaptiveThreshold(
+    #         src=blur,
+    #         maxValue=255,
+    #         adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
+    #         thresholdType=cv2.THRESH_BINARY,
+    #         blockSize=self.data["blockSize"],
+    #         C=self.data["C"],
+    #     )
+    #
+    #     self.dilation = cv2.dilate(
+    #         src=self.adaptive_gaussian_threshold.copy(),
+    #         kernel=self.data["KDil"],
+    #         iterations=self.data["iDil"],
+    #     )
+    #
+    #     # noise reduction in a binary image https://www.researchgate.net/publication/4376361_Noise_removal_and_enhancement_of_binary_images_using_morphological_operations
+    #
+    #     self.closing = cv2.morphologyEx(
+    #         src=self.adaptive_gaussian_threshold.copy(),
+    #         op=cv2.MORPH_CLOSE,
+    #         kernel=self.data["KClos"],
+    #         iterations=self.data["iClos"],
+    #     )
+    #
+    #     self.opening = cv2.morphologyEx(
+    #         src=self.adaptive_gaussian_threshold.copy(),
+    #         op=cv2.MORPH_OPEN,
+    #         kernel=self.data["KOpen"],
+    #         iterations=self.data["iOpen"],
+    #     )
+    #
+    #     K = 5
+    #     iterations = 7
+    #     self.dialte_opening = cv2.morphologyEx(
+    #         src=self.dilation.copy(),
+    #         op=cv2.MORPH_OPEN,
+    #         kernel=(K, K),
+    #         iterations=iterations,
+    #     )
+    #     self.dialte_closing = cv2.morphologyEx(
+    #         src=self.dilation.copy(),
+    #         op=cv2.MORPH_CLOSE,
+    #         kernel=(K, K),
+    #         iterations=iterations,
+    #     )
+    #
+    #     self.fill = fill(self.closing.copy())
+    #     self.fill2 = fill(self.opening.copy())
+    #     self.fill3 = fillCirc(self.fill2.copy())
+    #     # self.fill3 = fill(self.open_close.copy())
+    #     # self.fll2 = fillhole(self.opening)
+
+    # @staticmethod
+    def preprocessing2(self,image):
+        image.grayscale = cv2.cvtColor(image.image, cv2.COLOR_BGRA2GRAY)
+        image.hsv = cv2.cvtColor(image.image, cv2.COLOR_BGR2HSV)
+        (image.hue, image.saturation, image.intensity) = cv2.split(image.hsv)
+
+        image.blur(image.intensity, ksize=(3, 3), iterations=5)
+        image.adaptive_threshold(
+            image=image.blur,
+            blockSize=15,
+            C=3,
+        )
+        image.dilate(
+            image.adaptive_gaussian_threshold,
+            kernel=(5, 5),
+            iterations=7,
+        )
+        image.closing(
+            image.adaptive_gaussian_threshold,
+            kernel=(5, 5),
             iterations=10,
         )
-
-        self.dialte_opening = cv2.morphologyEx(
-            self.dilation.copy(),
-            cv2.MORPH_OPEN,
-            (Kc, Kc),
+        image.opening(
+            image.adaptive_gaussian_threshold,
+            kernel=(5, 5),
             iterations=7,
         )
-        self.dialte_closing = cv2.morphologyEx(
-            self.dilation.copy(),
-            cv2.MORPH_CLOSE,
-            (Kc, Kc),
-            iterations=7,
-        )
-        # h, w = self.opening.shape[:2]
-        # im_floodfill = self.opening.copy()
-        # mask = np.zeros((h + 2, w + 2), np.uint8)
-        #
-        # self.imfill = cv2.floodFill(im_floodfill, mask, (0, 0), 255)
-        # im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-        #
-        # Combine the two images to get the foreground.
-        # self.im_out = self.opening | im_floodfill_inv
-        #
-        # des = cv2.bitwise_not(self.closing.copy())
-        # contour, hier = cv2.findContours(des, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        # #
-        # for cnt in contour:
-        #     cv2.drawContours(des, [cnt], 0, 255, -1)
-        # #
-        # self.fill = cv2.bitwise_not(des)
-
-        self.fill = fill(self.closing.copy())
-        self.fill2 = fill(self.opening.copy())
-        self.fill3 = fillCirc(self.opening.copy())
-        # self.fill3 = fill(self.open_close.copy())
-        # self.fll2 = fillhole(self.opening)
+        # image.dialte_opening(image.dilation, kernel=(5, 5), iterations=7)
+        # image.dialte_closing(image.dilation, kernel=(5, 5), iterations=7)
+        image.fill = fill(image.closing.copy())
+        image.fill2 = fill(image.opening.copy())
+        image.fill3 = fillCirc(image.fill2.copy())
