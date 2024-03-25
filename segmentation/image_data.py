c55da910a05d40538e43ce50b40b6b790f1e89e4
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-03-25 11:05:52 +0000

    plaing about with dataclass for image data

---
+++ segmentation/image_data.py
@@ -0,0 +1,12 @@
+# from cv2 import MatLike
+# from dataclasses import dataclass, asdict
+#
+#
+# @dataclass
+# class ImageData(order=True):
+#     id: int
+#     image: MatLike
+#     operation: str
+#     params: str
+#     show: bool
+#     
