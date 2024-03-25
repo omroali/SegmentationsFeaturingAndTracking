3bbfdb054584c3f38aa62fbfeac8e9f4d4f62660
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-03-25 06:31:00 +0000

    introducing multiprocessing to speed up the run

--- seg_main.py
+++ seg_main.py
@@ -1,36 +1,14 @@
-# import cv2
 import os
-
-# from matplotlib import pyplot as plt
-
-# import numpy as np
-# import math
 from tqdm import tqdm
 
-# import segmentation
 from datetime import datetime
 from segmentation.image_segmentation import ImageSegmentation
 from segmentation.utils import get_images_in_path, show_image_list
 
+import multiprocessing as mp
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 path = "/home/kyro/Code/SegmentationsFeaturingAndTracking/data/ball_frames"
-
-
-# def store_image_data(image: ImageSegmentation, time: datetime):
-#     """method to store in a text file the image data for processing"""
-#     check_path = os.path.exists(f"process_data/{time}/data.txt")
-#     if not check_path:
-#         with open(f"process_data/{time}/data.txt", "w") as f:
-#             f.write(f"Kblur: {image.data['Kblur']}")
-#             f.write(f"blockSize: {image.data['blockSize']}")
-#             f.write(f"C: {image.data['C']}")
-#             f.write(f"KDil: {image.data['KDil']}")
-#             f.write(f"iDil: {image.data['iDil']}")
-#             f.write(f"KClos: {image.data['KClos']}")
-#             f.write(f"iClos: {image.data['iClos']}")
-#             f.write(f"KOpen: {image.data['KOpen']}")
-#             f.write(f"iOpen: {image.data['iOpen']}")
-#             f.write("\n\n")
 
 
 def store_image_data(image: ImageSegmentation, time: datetime):
@@ -42,36 +20,40 @@ def store_image_data(image: ImageSegmentation, time: datetime):
                 f.write(f"{entry}\n")
 
 
+def process_image(image_path, time: datetime, i: int):
+    """method to process the image"""
+    image = ImageSegmentation(image_path)
+    image.preprocessing2(image)
+    images = {
+        "original": image.image,
+        "intensity": image.intensity,
+        "adaptive_gaussian_threshold": image.adaptive_gaussian_threshold,
+        "dilation": image.dilation,
+        "closing": image.closing,
+        "opening": image.opening,
+        "floodfill_closing": image.fill,
+        "floodfill_opening": image.fill2,
+        # "dialte_closing": image.dialte_closing,
+        # "dialte_opening": image.dialte_opening,
+        "circular_fill": image.fill3,
+    }
+
+    show_image_list(
+        image_dict=images,
+        figsize=(10, 10),
+        save_path=f"process_data/{time}/img_{i}",
+    )
+
+    store_image_data(image, time)
+
+
 def main():
     images = get_images_in_path(path)
     time = datetime.now().isoformat("_", timespec="seconds")
     os.mkdir(f"process_data/{time}")
 
     for i, image in tqdm(enumerate(images), desc="Processing..:"):
-        image = ImageSegmentation(image)
-        image.preprocessing2(image)
-        images = {
-            "original": image.image,
-            "intensity": image.intensity,
-            "adaptive_gaussian_threshold": image.adaptive_gaussian_threshold,
-            "dilation": image.dilation,
-            "closing": image.closing,
-            "opening": image.opening,
-            "floodfill_closing": image.fill,
-            "floodfill_opening": image.fill2,
-            # "dialte_closing": image.dialte_closing,
-            # "dialte_opening": image.dialte_opening,
-            "circular_fill": image.fill3,
-        }
-
-        # print(images)
-        show_image_list(
-            image_dict=images,
-            figsize=(10, 10),
-            save_path=f"process_data/{time}/img_{i}",
-        )
-
-    store_image_data(image, time)
+        mp.Process(target=process_image, args=(image, time, i)).start()
 
 
 if __name__ == "__main__":
