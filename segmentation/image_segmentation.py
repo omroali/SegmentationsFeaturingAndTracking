824aabcd5257d9219d24300b98c847e57c9f5621
Kyro <49950899+Olseda20@users.noreply.github.com>
2024-04-01 16:25:11 +0100

    adding variable circularity

--- segmentation/image_segmentation.py
+++ segmentation/image_segmentation.py
@@ -65,7 +65,8 @@ class ImageSegmentation:
     def dilate(self, image, kernel=(3, 3), iterations=1, op=cv2.MORPH_ELLIPSE):
         """apply dilation to the image"""
         image = image.copy()
-        kernel = cv2.getStructuringElement(op, kernel)
+        if type(op) != np.ndarray:
+            kernel = cv2.getStructuringElement(op, kernel)
         dilation = cv2.dilate(
             src=image,
             kernel=kernel,
@@ -81,7 +82,8 @@ class ImageSegmentation:
     def erode(self, image, kernel=(3, 3), iterations=1, op=cv2.MORPH_ELLIPSE):
         """apply dilation to the image"""
         image = image.copy()
-        kernel = cv2.getStructuringElement(op, kernel)
+        if type(op) != np.ndarray:
+            kernel = cv2.getStructuringElement(op, kernel)
         dilation = cv2.erode(
             src=image,
             kernel=kernel,
@@ -94,10 +96,11 @@ class ImageSegmentation:
         )
         return dilation
 
-    def closing(self, image, kernel=(5, 5), iterations=10):
+    def closing(self, image, kernel=(5, 5), iterations=10, op=cv2.MORPH_ELLIPSE):
         """apply closing to the image"""
         image = image.copy()
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel)
+        if type(op) != np.ndarray:
+            kernel = cv2.getStructuringElement(op, kernel)
         closing = cv2.morphologyEx(
             src=image,
             op=cv2.MORPH_CLOSE,
@@ -114,7 +117,8 @@ class ImageSegmentation:
     def opening(self, image, kernel=(5, 5), iterations=1, op=cv2.MORPH_ELLIPSE):
         """apply opening to the image"""
         image = image.copy()
-        kernel = cv2.getStructuringElement(op, kernel)
+        if type(op) != np.ndarray:
+            kernel = cv2.getStructuringElement(op, kernel)
         opening = cv2.morphologyEx(
             src=image,
             op=cv2.MORPH_OPEN,
@@ -153,7 +157,7 @@ class ImageSegmentation:
         )
         return image
 
-    def fill_image(self, image_data, name, show=True):
+    def fill_image(self, image_data, name, show=False):
         self.log_image_processing(
             image_data[name],
             f"fill_{name}",
@@ -202,11 +206,12 @@ class ImageSegmentation:
     #     # cv2.destroyAllWindows()
     #     return img
 
-    def find_ball_contours(self, image, circ_thresh, min_area=400, max_area=4900):
+    def find_ball_contours(
+        self, image, circ_thresh, alpha=1, min_area=400, max_area=4900, verbose=False
+    ):
         img = image.copy()
         cnts = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
         blank_image = np.zeros(img.shape, dtype=img.dtype)
 
         # Loop over contours
@@ -221,24 +226,42 @@ class ImageSegmentation:
             if area == 0:
                 continue  # Skip to the next iteration if area is zero
             # Filter for circular shapes
-            radius = math.sqrt(area / math.pi)
-            # circ = int(2 * math.pi * radius)
-
             circularity = 4 * math.pi * area / (peri**2)
 
-            if (
-                (len(approx) > 10)
-                and (area > min_area and area < max_area)
-                and circularity > circ_thresh
-            ):
+            # circularity_constant = (
+            #     1 / (1 - circ_thresh / (max_area)) * (area + min_area) + circ_thresh
+            # )
+            if len(approx) < 5 or area < min_area or area > max_area:
+                continue
+
+            if circularity < circ_thresh:
+                # circ_thresh_2 = circ_thresh - 0.15
+                circularity_constant = (
+                    -(1 - circ_thresh) / (max_area - min_area) * (area) + circ_thresh
+                ) / alpha
+
+                if circularity < circ_thresh - circularity_constant:
+                    # print(
+                    #     f"rejected area {area}, circularity_constant {circularity_constant}, circularity {circularity}"
+                    # )
+                    continue
+
+                # print(
+                #     f"area {area}, circularity_constant {circularity_constant}, circularity {circularity}"
+                # )
+                #
+                # if verbose:
                 # print("\ndetected")
                 # print("approx", len(approx))
                 # print("area", area)
                 # print("peri", peri)
-                # print("circ", circ)
+                # if circularity < circ_thresh + 0.1:
+                # print(
+                #     f"circ: {circularity}, area: {area}, circularity_constant: {circularity_constant}"
+                # )
                 # if len(approx) < circ + 10 and len(approx) > circ - 10:
-                cv2.drawContours(blank_image, [c], -1, (255), cv2.FILLED)
-                # cv2.ellipse(blank_image, c, (255), 2)
+            cv2.drawContours(blank_image, [c], -1, (255), cv2.FILLED)
+            # cv2.ellipse(blank_image, c, (255), 2)
         # cv2.imshow("cnts", blank_image)
         # cv2.waitKey(0)
 
@@ -247,50 +270,12 @@ class ImageSegmentation:
         return blank_image
 
     @staticmethod
-    def evaluating_effect_of_blocksize(image, C):
+    def preprocessing(image):
         image_data = {}
+
         image_data["original"] = {
             "image": image.image,
-            "operation": "original",
             "show": False,
-        }
-
-        (_, _, intensity) = cv2.split(cv2.cvtColor(image.image, cv2.COLOR_BGR2HSV))
-        image_data["intensity"] = {
-            "image": cv2.threshold(
-                image_data["intensity"], 100, 255, cv2.THRESH_BINARY_INV
-            ),
-            "show": True,
-        }
-        # image_data["blur"] = {
-        #     "image": image.blur(
-        #         image_data["intensity"]["image"], ksize=(3, 3), iterations=2
-        #     ),
-        #     "operation": "blur",
-        #     "params": image.processing_data[-1],
-        #     "show": True,
-        # }
-
-        for blockSize in range(5, 21, 2):
-            name = f"adap_gaus_thrsh_bS_{blockSize}_C_{C}"
-            image_data[name] = {
-                "image": image.adaptive_threshold(
-                    image=image_data["blur"]["image"].copy(),
-                    blockSize=blockSize,
-                    C=C,
-                ),
-                "show": False,
-            }
-
-            image.fill_image(image_data, name)
-
-    @staticmethod
-    def preprocessing2(image):
-        image_data = {}
-
-        image_data["original"] = {
-            "image": image.image,
-            "show": True,
         }
         image_data["grayscale"] = {
             "image": cv2.cvtColor(image.image, cv2.COLOR_BGRA2GRAY),
@@ -324,7 +309,7 @@ class ImageSegmentation:
 
         image_data["intensity_threshold"] = {
             "image": intensity_threshold,
-            "show": True,
+            "show": False,
         }
 
         name = "adap_gaus_thrsh"
@@ -334,20 +319,10 @@ class ImageSegmentation:
                 blockSize=19,
                 C=5,
             ),
-            "show": True,
+            "show": False,
         }
 
         image.fill_image(image_data, name)
-
-        # contours = image.find_ball_contours(
-        #     cv2.bitwise_not(image_data[f"fill_{name}"]["image"])
-        # )
-        # image_data["contours"] = {
-        #     "image": contours,
-        #     "operation": "",
-        #     "params": "",
-        #     "show": True,
-        # }
 
         image_data["open"] = {
             "image": image.opening(
@@ -367,39 +342,8 @@ class ImageSegmentation:
             "show": False,
         }
         image.fill_image(image_data, "erode")
+
         image_data["dilate_and_erode"] = {
-            "image": image.dilate_and_erode(
-                image_data["fill_erode"]["image"].copy(),
-                k_d=5,
-                i_d=3,
-                k_e=10,
-                i_e=1,
-                iterations=3,
-                op=cv2.MORPH_CROSS,
-            ),
-            "show": False,
-        }
-        image_data["dilate_and_erode_2"] = {
-            "image": image.dilate_and_erode(
-                image_data["fill_erode"]["image"].copy(),
-                k_d=6,
-                i_d=3,
-                k_e=7,
-                i_e=2,
-                iterations=2,
-                # op=cv2.MORPH_CROSS,
-            ),
-            "show": False,
-        }
-        image_data["dilate_2"] = {
-            "image": image.dilate(
-                image=image_data["dilate_and_erode"]["image"].copy(),
-                kernel=(4, 4),
-                iterations=5,
-            ),
-            "show": False,
-        }
-        image_data["dilate_and_erode_3"] = {
             "image": image.dilate_and_erode(
                 image_data["fill_erode"]["image"].copy(),
                 k_d=4,
@@ -415,25 +359,25 @@ class ImageSegmentation:
         image_data["segmentation_with_threshold"] = {
             "image": cv2.bitwise_not(
                 cv2.bitwise_or(
-                    # cv2.bitwise_not(
                     image_data["intensity_threshold"]["image"],
-                    # ),
-                    # cv2.bitwise_not(
-                    image_data["dilate_and_erode_3"]["image"],
+                    image_data["dilate_and_erode"]["image"],
                 ),
             ),
             "show": False,
         }
 
         contours = image.find_ball_contours(
-            # cv2.bitwise_not(image_data["dilate_and_erode_3"]["image"])
-            cv2.bitwise_not(image_data["dilate_and_erode_3"]["image"]),
-            0.2,
+            cv2.bitwise_not(image_data["dilate_and_erode"]["image"]),
+            circ_thresh=0.3,
+            alpha=1,
+            min_area=400,
+            max_area=4900,
+            verbose=False,
         )
 
         image_data["contours"] = {
             "image": contours,
-            "show": True,
+            "show": False,
         }
 
         image_data["im_1"] = {
@@ -456,56 +400,173 @@ class ImageSegmentation:
                     image_data["im_1"]["image"], image_data["im_2"]["image"]
                 ),
             ),
-            "show": True,
+            "show": False,
         }
-
-        image_data["opening_after_segmentation"] = {
-            "image": image.opening(
-                image_data["segmentation_before_recontour"]["image"],
-                kernel=(3, 3),
-                iterations=6,
-            ),
-            "show": True,
-        }
+        # image_data["dilate_after_opening_after_segmentation"] = {
+        #     "image": image.dilate(
+        #         image_data["segmentation_before_recontour"]["image"],
+        #         kernel=(4, 4),
+        #         iterations=3,
+        #     ),
+        #     "show": True,
+        # }
+        # image_data["opening_after_segmentation"] = {
+        #     "image": image.opening(
+        #         image_data["dilate_after_opening_after_segmentation"]["image"],
+        #         kernel=(5, 5),
+        #         iterations=5,
+        #     ),
+        #     "show": True,
+        # }
         # image_data["erode_opening_after_segmentation"] = {
         #     "image": image.erode(
         #         image_data["opening_after_segmentation"]["image"],
         #         kernel=(3, 3),
         #         iterations=4,
         #     ),
-        #     "show": False,
+        #     "show": True,
         # }
         #
-        # image_data["closing_after_opening_after_segmentation"] = {
+        circular_se = np.array(
+            [
+                [0, 0, 0, 1, 0, 0, 0],
+                [0, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 1, 1, 0],
+                [0, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 1, 0, 0, 0],
+            ],
+            dtype="uint8",
+        )
+        image_data["erode_after_segmentation"] = {
+            "image": image.erode(
+                image_data["segmentation_before_recontour"]["image"],
+                kernel=(3, 3),
+                iterations=2,
+                # op=cv2.MORPH_RECT,
+            ),
+            "show": False,
+        }
+
+        image_data["dilate_after_erode_after_segmentation"] = {
+            "image": image.dilate(
+                image_data["erode_after_segmentation"]["image"],
+                kernel=(2, 2),
+                iterations=8,
+                op=circular_se,
+            ),
+            "show": False,
+        }
+
+        # image_data["closing_after_dilate_after_erode_after_segmentation"] = {
         #     "image": image.closing(
-        #         image_data["opening_after_segmentation"]["image"],
-        #         kernel=(5, 5),
-        #         iterations=2,
+        #         image_data["dilate_after_erode_after_segmentation"]["image"],
+        #         kernel=(7, 7),
+        #         iterations=4,
+        #         op=circular_se,
         #     ),
         #     "show": True,
         # }
 
-        image_data["dilate_after_opening_after_segmentation"] = {
+        image_data["opening_after_dilate_after_erode_after_segmentation"] = {
+            "image": image.opening(
+                image_data["dilate_after_erode_after_segmentation"]["image"],
+                kernel=(7, 7),
+                iterations=3,
+                op=circular_se,
+            ),
+            "show": False,
+        }
+
+        image_data[
+            "erode_after_opening_after_dilate_after_erode_after_segmentation"
+        ] = {
+            "image": image.erode(
+                image_data["opening_after_dilate_after_erode_after_segmentation"][
+                    "image"
+                ],
+                kernel=(3, 3),
+                iterations=7,
+                # op=circular_se,
+            ),
+            "show": True,
+        }
+
+        circular_se = np.array(
+            [
+                [0, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 1, 1, 0],
+            ]
+        )
+
+        image_data[
+            "dilate_after_erode_after_opening_after_dilate_after_erode_after_segmentation"
+        ] = {
             "image": image.dilate(
-                image_data["opening_after_segmentation"]["image"],
+                image_data[
+                    "erode_after_opening_after_dilate_after_erode_after_segmentation"
+                ]["image"],
                 kernel=(3, 3),
                 iterations=5,
+                # op=circular_se,
             ),
             "show": True,
         }
-        # image_data["erode_dilate_after_segmentation"] = {
-        #     "image": image.erode(
-        #         image_data["dilate_after_segmentation"]["image"],
-        #         kernel=(3, 3),
-        #         iterations=5,
+
+        # image_data["opening_after_segmentation_2"] = {
+        #     "image": image.opening(
+        #         image_data["dilate_after_opening_after_segmentation"]["image"],
+        #         kernel=(2, 2),
+        #         iterations=10,
         #     ),
         #     "show": True,
         # }
 
-        image_data["segmentation"] = {
-            "image": image.find_ball_contours(
-                image_data["erode_dilate_after_segmentation"]["image"], 0.36, 150, 5000
+        thresh = 1
+        min_area = 150
+        for i in range(1, 3):
+            i = i
+            image_data[f"segmentation_thresh_{thresh}_alpha_{i}"] = {
+                "image": image.find_ball_contours(
+                    image_data[
+                        "dilate_after_erode_after_opening_after_dilate_after_erode_after_segmentation"
+                    ]["image"],
+                    circ_thresh=thresh,  # range betwen .65 and .7
+                    alpha=i,
+                    min_area=min_area,
+                    max_area=3500,
+                    verbose=True,
+                ),
+                "show": True,
+            }
+
+        image_data[f"segmentation"] = {
+            "image": image.dilate(
+                image_data[f"segmentation_thresh_{thresh}_alpha_1"]["image"],
+                kernel=(3, 3),
+                iterations=3,
+                # op=circular_se,
             ),
             "show": True,
         }
+
+        # image_data["segmentation"] = {
+        #     "image": image.find_ball_contours(
+        #         image_data[
+        #             "dilate_after_erode_after_opening_after_dilate_after_erode_after_segmentation"
+        #         ]["image"],
+        #         circ_thresh=thresh,  # range betwen .65 and .7
+        #         alpha=3,
+        #         min_area=min_area,
+        #         max_area=3500,
+        #         verbose=True,
+        #     ),
+        #     "show": False,
+        # }
+
         return image_data
