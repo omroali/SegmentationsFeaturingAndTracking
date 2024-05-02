967708478c1528c535d42dc45f46d453fe7958e6
omro <49950899+Olseda20@users.noreply.github.com>
2024-05-02 16:17:26 +0100

    optimisation stuff and report start

--- tracking_main.py
+++ tracking_main.py
@@ -1,95 +1,210 @@
-
 from matplotlib import pyplot as plt
 import numpy as np
 
+
 def kalman_predict(x, P, F, Q):
-    xp = F*x
-    Pp = F*P*F.T + Q
+    xp = F * x
+    Pp = F * P * F.T + Q
     return xp, Pp
 
-def kalman_update(x, P, H, R, z):
-    S = H*P*H.T + R
-    K = P*H.T*np.linalg.inv(S)
-    zp = H*x
 
-    xe = x + K*(z - zp)
-    Pe = P - K*H*P
+def kalman_update(x, P, H, R, z):
+    S = H * P * H.T + R
+    K = P * H.T * np.linalg.inv(S)
+    zp = H * x
+
+    xe = x + K * (z - zp)
+    Pe = P - K * H * P
     return xe, Pe
 
-def kalman_tracking(z, dt = 0.5, kq = 0.16, kr = 0.25):
+
+def kalman_tracking(
+    z,
+    x01=0.0,
+    x02=0.0,
+    x03=0.0,
+    x04=0.0,
+    dt=0.5,
+    nx=16,
+    ny=0.36,
+    nvx=0.16,
+    nvy=0.36,
+    nu=0.25,
+    nv=0.25,
+):
     # Constant Velocity
-    F = np.matrix([[1, dt, 0, 0],
-                    [0, 1, 0, 0],
-                    [0, 0, 1, dt],
-                    [0, 0, 0, 1]])
+    F = np.matrix([[1, dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, dt], [0, 0, 0, 1]])
 
     # Cartesian observation model
-    H = np.matrix([[1, 0, 0, 0],
-                   [0, 0, 1, 0]])
-    
+    H = np.matrix([[1, 0, 0, 0], [0, 0, 1, 0]])
 
-    Q = kq*np.matrix([[1, 0, 0, 0],
-                   [0, 2, 0, 0],
-                   [0, 0, 1, 0],
-                   [0, 0, 0, 2]])
-    
-    R = kr*np.matrix([[1, 0],
-                   [0, 1]])
+    # Motion Model
+    Q = np.matrix([[nx, 0, 0, 0], [0, nvx, 0, 0], [0, 0, ny, 0], [0, 0, 0, nvy]])
 
-    x = np.matrix([0, 0, 0, 0]).T
+    R = np.matrix([[nu, 0], [0, nv]])
+
+    x = np.matrix([x01, x02, x03, x04]).T
     P = Q
 
     N = len(z[0])
-    s = np.zeros((4,N)) 
+    s = np.zeros((4, N))
 
     for i in range(N):
         xp, Pp = kalman_predict(x, P, F, Q)
-        x, P = kalman_update(xp, Pp, H, R, z[:,i])
-        val = np.array(x[:2,:2]).flatten()
-        s[:,i] = val
-    
-    px = s[0,:]
-    py = s[1,:]
+        x, P = kalman_update(xp, Pp, H, R, z[:, i])
+        val = np.array(x[:2, :2]).flatten()
+        s[:, i] = val
+
+    px = s[0, :]
+    py = s[2, :]
 
     return px, py
 
+
+def error(x, y, px, py):
+    err = []
+    for i in range(len(x)):
+        err.append(np.sqrt((x[i] - px[i]) ** 2 + (y[i] - py[i]) ** 2))
+    return err
+
+
+def optimisation(trial, x, y, z, dt, nx, ny, nvx, nvy, nu, nv, x01, x02, x03, x04):
+    # Q
+    nx = trial.suggest_float("nx", 0.0, 3000.0)
+    ny = trial.suggest_float("ny", 0.0, 7200.0)
+    nvx = trial.suggest_float("nvx", 0.0, 3200.0)
+    nvy = trial.suggest_float("nvy", 0.0, 7200.0)
+
+    # R
+    nu = trial.suggest_float("nu", -25.0, 25.0)
+    nv = trial.suggest_float("nv", -25.0, 25.0)
+
+    # init x
+    x01 = trial.suggest_float("x01", -0.0, 1000.0)
+    x02 = trial.suggest_float("x02", -0.0, 1000.0)
+    x03 = trial.suggest_float("x03", -0.0, 1000.0)
+    x04 = trial.suggest_float("x04", -0.0, 1000.0)
+
+    px, py = kalman_tracking(z, x01, x02, x03, x04, dt, nx, ny, nvx, nvy, nu, nv)
+    rms_val = rms(x, y, px, py)
+    return rms_val
+
+
+def rms(x, y, px, py):
+    err = np.array(error(x, y, px, py))
+    return np.sqrt(err.mean())
+
+
+def optimize_rms(x, y, z):
+    import optuna
+    from tqdm import tqdm
+
+    trials = 100000
+
+    pbar = tqdm(total=trials, desc="Optimization Progress")
+
+    def print_new_optimal(study, trial):
+        # Check if the trial is better than the current best
+        pbar.update(1)
+        if trial.value == study.best_value:
+            print(f"New Best RMS: {trial.value} (trial number {trial.number})")
+            print("Best parameters:", study.best_params)
+
+    optuna.logging.set_verbosity(optuna.logging.WARNING)
+
+    study = optuna.create_study()
+    dt = 0.5
+    nx = 0.16
+    ny = 0.36
+    nvx = 0.16
+    nvy = 0.36
+    nu = 0.25
+    nv = 0.25
+    x01 = 0.0
+    x02 = 0.0
+    x03 = 0.0
+    x04 = 0.0
+    #
+    # nx = -90.0
+    # ny = 203.0
+    # nvx = -13.0
+    # nvy = 720.0
+    # nu = -23.0
+    # nv = -5.0
+    # x01 = 190.7
+    # x02 = 43.2
+    # x03 = -842.5
+    # x04 = 38.58
+
+    # nx = 265.6984895041111
+    # ny = 4377.212482966368
+    # nvx = 0.1650447124433483
+    # nvy = 694.8165035716708
+    # nu = 23.167412752765156
+    # nv = -13.172081648387536
+    # x01 = -372.1342841527826
+    # x02 = 441.2537740166025
+    # x03 = 395.294325684342
+    # x04 = -37.807362565797575
+
+    study.optimize(
+        lambda trial: optimisation(
+            trial, x, y, z, dt, nx, ny, nvx, nvy, nu, nv, x01, x02, x03, x04
+        ),
+        n_trials=trials,
+        n_jobs=8,
+        callbacks=[print_new_optimal],  # Add the callback here
+    )
+
+    return study.best_params
+
+
 if __name__ == "__main__":
 
-    x  = np.genfromtxt('data/x.csv',  delimiter=',')
-    y  = np.genfromtxt('data/y.csv',  delimiter=',')
-    na = np.genfromtxt('data/na.csv', delimiter=',')
-    nb = np.genfromtxt('data/nb.csv', delimiter=',')
+    x = np.genfromtxt("data/x.csv", delimiter=",")
+    y = np.genfromtxt("data/y.csv", delimiter=",")
+    na = np.genfromtxt("data/na.csv", delimiter=",")
+    nb = np.genfromtxt("data/nb.csv", delimiter=",")
     z = np.stack((na, nb))
-    plt.plot(x, y)
-    px, py = kalman_tracking(z)
-    lg = ['True', 'Kalman']
-    for kr in [0.01, 0.1, 1, 10, 100, 1000, 10000]:
-        for kq in [0.01]:
-                #    , 0.1 , 1, 10, 100, 1000, 10000]:
-            px, py = kalman_tracking(z, dt = 0.5, kr = kr, kq = kq)
-            plt.plot(px, py)
-            lg.append('kr = ' + str(kr) + ', kq = ' + str(kq))
-    
-    plt.legend(lg)
 
-    # for dt in [0.5]:
-    #     # for kq in 10*[0.16, 0.14, 0.12, 0.10]:
-    #         for kr in 10*[0.25, 0.20, 0.15, 0.10]:
-    #             px, py = kalman_tracking(z, dt = dt, kr = kr)
-    #             plt.plot(px, py)
+    optimize_rms(x, y, z)
+
+    nx = 74.16461149558424
+    ny = 5116.321061403971
+    nvx = 9.610652449262691
+    nvy = 550.9527069217193
+    nu = 19.438996908883986
+    nv = -12.609141060408131
+    x01 = 186.81864611999316
+    x02 = 45.69362782835684
+    x03 = -401.45334566191826
+    x04 = -87.5946780256347
+
+    nx = 265.6984895041111
+    ny = 4377.212482966368
+    nvx = 0.1650447124433483
+    nvy = 694.8165035716708
+    nu = 23.167412752765156
+    nv = -13.172081648387536
+    x01 = -372.1342841527826
+    x02 = 441.2537740166025
+    x03 = 395.294325684342
+    x04 = -37.807362565797575
+
+    px, py = kalman_tracking(
+        nx=nx,
+        ny=ny,
+        nvx=nvx,
+        nvy=nvy,
+        nu=nu,
+        nv=nv,
+        x01=x01,
+        x02=x02,
+        x03=x03,
+        x04=x04,
+        z=z,
+    )
+    plt.plot(x, y)
+    plt.plot(px, py)
     plt.scatter(na, nb)
     plt.show()
-
-    import optuna
-
-    def objective(trial):
-        kr = trial.suggest_uniform('kr', 0.0, 1.0)
-        kq = trial.suggest_uniform('kq', 0.0, 1.0)
-
-        # Replace this with your actual Kalman filter code
-        # and a metric that measures its performance
-        # For example, you could run the filter on some test data
-        # and compute the mean squared error of the estimates
-        mse = run_kalman_filter(kr, kq)
-
-        return mse
